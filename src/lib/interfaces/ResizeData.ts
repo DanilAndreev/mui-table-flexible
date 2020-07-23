2c8c37dc04cdb4ccc247091922c261c87a77a108
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-07-23 11:25:33 +0300

    formatted code

--- src/lib/interfaces/ResizeData.ts
+++ src/lib/interfaces/ResizeData.ts
@@ -1,6 +1,7 @@
 import {SystemResizeable} from "./SystemResizeable";
 
-export interface ResizeData extends SystemResizeable{
+export interface ResizeData
+    extends SystemResizeable{
     /**
      * systemWidth - thi width of the component, stored in table
      * @type number
